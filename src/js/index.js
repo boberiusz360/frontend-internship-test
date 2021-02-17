47646fcc7da479439d9109bbea04b36aab43be9b
JA <bartek.zag40@gmail.com>
2021-02-18 00:12:21 +0100

    Task 1 ;)

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,32 @@
 /* Here goes your JS code */
+/* Here I go... */
+
+//var loaded = false;
+
+// vPos - vertical position hPos - horizontal position
+function hide(loaded)
+{
+	if(!loaded)
+	{
+		var vPos = window.innerHeight;
+		vPos += 350;
+		vPos -= 2 * vPos;
+		var output = vPos + "px";
+		document.getElementById("theOne").style.top = output;
+		loaded = true;
+	}
+	return;
+}
+
+function popPopup()
+{
+	var vPos = window.innerHeight;
+	var vOutput = (vPos - 350) / 2;
+	vOutput += "px";
+	var hPos = window.innerWidth;
+	hPos = (hPos - 300) / 2;
+	var hOutput = hPos + "px";
+	document.getElementById("theOne").style.top = vOutput;
+	document.getElementById("theOne").style.left = hOutput;
+	return;
+}
