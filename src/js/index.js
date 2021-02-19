ca0b0e2bdcda339cb5ef5945f59cfacaa2761759
JA <bartek.zag40@gmail.com>
2021-02-19 02:46:17 +0100

    Task 3

--- src/js/index.js
+++ src/js/index.js
@@ -3,21 +3,51 @@
 
 // vPos - vertical position hPos - horizontal position
 
-
-var wait = false;
+function end()
+{
+	hide();
+	document.getElementById("main").innerHTML += "Thank You!";
+	document.getElementById("show-popup-form").style.position = "absolute";
+	document.getElementById("show-popup-form").style.top = "-1000px";
+	sessionStorage.setItem("wait", false);
+}
 
 function hide()
 {
-	if(wait == false)
+	var vPos = window.innerHeight;
+	vPos += 350;
+	vPos -= 2 * vPos;
+	var output = vPos + "px";
+	document.getElementById("theOne").style.top = output;
+	document.getElementById("impostor").style.animation = "impostorEscape 0.5s";
+	window.setTimeout(impostorEscape, 500, output);
+	function impostorEscape(output)
 	{
-		var vPos = window.innerHeight;
-		vPos += 350;
-		vPos -= 2 * vPos;
-		var output = vPos + "px";
-		document.getElementById("theOne").style.top = output;
+		document.getElementById("impostorFrame").style.top = output;
+	}
+	var wait = sessionStorage.getItem("wait");
+	if(wait)
+	{
+		popPopup();
+		document.forms["popupForm"]["login"].value = sessionStorage.getItem("email");
+		document.forms["popupForm"]["password"].value = sessionStorage.getItem("password");
+		document.forms["popupForm"]["Greg"].checked = true;
+		sessionStorage.removeItem("wait");
+		window.setTimeout(end, 3000);
 	}
 	return;
 }
+
+function popPopupEnd(Voutput, Houtput)
+{
+	document.getElementById("theOne").style.top = Voutput;
+	document.getElementById("theOne").style.left = Houtput;
+	/*document.getElementById('impostor').style.width = "300px";
+	document.getElementById('impostor').style.height = "350px";
+	document.getElementById('impostor').style.top = "0px";
+	document.getElementById('impostor').style.left = "0px";*/
+}
+
 
 function popPopup()
 {
@@ -27,16 +57,24 @@ function popPopup()
 	var hPos = window.innerWidth;
 	hPos = (hPos - 300) / 2;
 	var hOutput = hPos + "px";
-	document.getElementById("theOne").style.top = vOutput;
-	document.getElementById("theOne").style.left = hOutput;
+	document.getElementById("impostorFrame").style.top = vOutput;
+	document.getElementById("impostorFrame").style.left = hOutput;
+	document.getElementById('impostor').style.animation = "impostorRising 0.5s";
+	window.setTimeout(popPopupEnd, 500, vOutput, hOutput);
 	return;
 }
 
 function doThings()
 {
+	hide();
 	var email = document.forms["popupForm"]["login"].value;
+	sessionStorage.setItem("email", email);
 	var pass = document.forms["popupForm"]["password"].value;
+	sessionStorage.setItem("password", pass);
 	var greg = document.forms["popupForm"]["Greg"].checked;
-	if((email != "")&&(pass != "")&&(greg)){alert(email);}
+	if((email != "")&&(pass != "")&&(greg))
+	{
+		sessionStorage.setItem("wait", true);
+	}
 	return;
 }
